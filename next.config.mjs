f62f7156dd54dc7d8a6591d6ae8e0a3f423421d2
abel-otegbola <abel15655@gmail.com>
2025-05-01 09:33:21 +0100

    fix: disable swc in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        forceSwcTransforms: false, // Disable SWC (uses Babel instead)
+    },
+    swcMinify: false, // Disable SWC minifier
 };
 
 export default nextConfig;
