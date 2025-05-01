ebb8a484372c96f9c6f8be253f29b55e905820c9
abel-otegbola <abel15655@gmail.com>
2025-05-01 16:12:27 +0100

    fix: next config error

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
 const nextConfig = {
     experimental: {
         forceSwcTransforms: false, // Disable SWC (uses Babel instead)
-    },
-    swcMinify: false, // Disable SWC minifier
+    }
 };
 
 export default nextConfig;
