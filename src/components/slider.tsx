5e86768c429972b2946d4c8c51db26ef08586d00
abel-otegbola <abel15655@gmail.com>
2025-07-04 21:16:04 +0100

    fix: reduce space between info

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -4,13 +4,13 @@ export default function Slider() {
             className="w-full gap-8 inline-flex flex-nowrap overflow-hidden"
         >
             <ul className="w-[1328px] flex items-center sm:text-[14px] text-[14px] justify-center gap-8 md:justify-start animate-infinite-scroll">
-                <li className="min-w-[310px]">
+                <li className="min-w-[305px]">
                     <p>This drop has completely sold out</p>
                 </li>
                 <li className="min-w-[290px]">
                     <p>Stay tuned for our next release</p>
                 </li>
-                <li className="min-w-[310px]">
+                <li className="min-w-[305px]">
                     <p>This drop has completely sold out</p>
                 </li>
                 <li className="min-w-[290px]">
@@ -18,13 +18,13 @@ export default function Slider() {
                 </li>
             </ul>     
             <ul className="w-[1296px] flex items-center sm:text-[14px] text-[14px] justify-center gap-8 md:justify-start animate-infinite-scroll">
-                <li className="min-w-[310px]">
+                <li className="min-w-[305px]">
                     <p>This drop has completely sold out</p>
                 </li>
                 <li className="min-w-[290px]">
                     <p>Stay tuned for our next release</p>
                 </li>
-                <li className="min-w-[310px]">
+                <li className="min-w-[305px]">
                     <p>This drop has completely sold out</p> 
                     {/* Performane meets style 210px */}
                 </li>
