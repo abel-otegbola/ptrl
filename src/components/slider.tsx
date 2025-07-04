e1e26b909b9faa5ab9c01ef7b3148b0b80629c44
abel-otegbola <abel15655@gmail.com>
2025-07-04 21:10:30 +0100

    update: sold out products info added to slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -4,31 +4,33 @@ export default function Slider() {
             className="w-full gap-8 inline-flex flex-nowrap overflow-hidden"
         >
             <ul className="w-[1328px] flex items-center sm:text-[14px] text-[14px] justify-center gap-8 md:justify-start animate-infinite-scroll">
-                <li className="min-w-[210px]">
-                    <p>Performance meets style</p>
+                <li className="min-w-[310px]">
+                    <p>This drop has completely sold out</p>
                 </li>
-                <li className="min-w-[400px]">
-                    <p>Shipping Available to Lagos | Ibadan | Abuja | Ogun</p>
+                <li className="min-w-[290px]">
+                    <p>Stay tuned for our next release</p>
                 </li>
-                <li className="min-w-[210px]">
-                    <p>Performance meets style</p>
+                <li className="min-w-[310px]">
+                    <p>This drop has completely sold out</p>
                 </li>
-                <li className="min-w-[400px]">
-                    <p>Shipping Available to Lagos | Ibadan | Abuja | Ogun</p>
+                <li className="min-w-[290px]">
+                    <p>Stay tuned for our next release</p>
                 </li>
             </ul>     
             <ul className="w-[1296px] flex items-center sm:text-[14px] text-[14px] justify-center gap-8 md:justify-start animate-infinite-scroll">
-                <li className="min-w-[210px]">
-                    <p>Performance meets style</p>
+                <li className="min-w-[310px]">
+                    <p>This drop has completely sold out</p>
                 </li>
-                <li className="min-w-[400px]">
-                    <p>Shipping Available to Lagos | Ibadan | Abuja | Ogun</p>
+                <li className="min-w-[290px]">
+                    <p>Stay tuned for our next release</p>
                 </li>
-                <li className="min-w-[210px]">
-                    <p>Performane meets style</p>
+                <li className="min-w-[310px]">
+                    <p>This drop has completely sold out</p> 
+                    {/* Performane meets style 210px */}
                 </li>
-                <li className="min-w-[400px]">
-                    <p>Shipping Available to Lagos | Ibadan | Abuja | Ogun</p>
+                <li className="min-w-[290px]">
+                    <p>Stay tuned for our next release</p>
+                    {/* Shipping Available to Lagos | Ibadan | Abuja | Ogun 400px */}
                 </li>
             </ul>               
         </div>
