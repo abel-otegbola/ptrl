e3a98c998b0c00bb897405997ee3212c07474bed
abel-otegbola <abel15655@gmail.com>
2025-09-27 20:03:31 +0100

    feat: make all products available

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -4,33 +4,26 @@ export default function Slider() {
             className="w-full gap-8 inline-flex flex-nowrap overflow-hidden"
         >
             <ul className="w-[1328px] flex items-center sm:text-[14px] text-[14px] justify-center gap-8 md:justify-start animate-infinite-scroll">
-                <li className="min-w-[305px]">
-                    <p>This drop has completely sold out</p>
+                <li className="min-w-[420px]">
+                    <p>Shipping Available To Lagos | Ibadan | Abuja | Ogun</p>
                 </li>
-                <li className="min-w-[290px]">
-                    <p>Stay tuned for our next release</p>
+                <li className="min-w-[420px]">
+                    <p>Shipping Available To Lagos | Ibadan | Abuja | Ogun</p>
                 </li>
-                <li className="min-w-[305px]">
-                    <p>This drop has completely sold out</p>
-                </li>
-                <li className="min-w-[290px]">
-                    <p>Stay tuned for our next release</p>
+                <li className="min-w-[420px]">
+                    <p>Shipping Available To Lagos | Ibadan | Abuja | Ogun</p>
                 </li>
             </ul>     
             <ul className="w-[1296px] flex items-center sm:text-[14px] text-[14px] justify-center gap-8 md:justify-start animate-infinite-scroll">
-                <li className="min-w-[305px]">
-                    <p>This drop has completely sold out</p>
+                <li className="min-w-[420px]">
+                    <p>Shipping Available To Lagos | Ibadan | Abuja | Ogun</p>
                 </li>
-                <li className="min-w-[290px]">
-                    <p>Stay tuned for our next release</p>
+                <li className="min-w-[420px]">
+                    <p>Shipping Available To Lagos | Ibadan | Abuja | Ogun</p>
                 </li>
-                <li className="min-w-[305px]">
-                    <p>This drop has completely sold out</p> 
+                <li className="min-w-[420px]">
+                    <p>Shipping Available To Lagos | Ibadan | Abuja | Ogun</p> 
                     {/* Performane meets style 210px */}
-                </li>
-                <li className="min-w-[290px]">
-                    <p>Stay tuned for our next release</p>
-                    {/* Shipping Available to Lagos | Ibadan | Abuja | Ogun 400px */}
                 </li>
             </ul>               
         </div>
